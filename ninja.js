21ad33571502cee386da2ae8528716de39c76503
Artem Nagornyi <anahorny@gmail.com>
2014-07-26 13:43:26 +0900

    uajpnews feeder ninja script

---
+++ ninja.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+(function() {
+	var fn = document.createElement('script'); fn.type = 'text/javascript';
+	fn.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feederninja.com/api/feed/6f1cf5d7cf184e1ebba5b970ebf346a3?fnurl=' + window.location.href;
+	var s = document.getElementsByTagName('head')[0].appendChild(fn);
+})();
+</script>
